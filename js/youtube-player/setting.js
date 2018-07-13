26c25dacb63aa908b9d878b932ae8ab12ad5ebd0
Matias Izura <lic.matiasizura@gmail.com>
2018-07-12 22:48:06 -0300

    AAA

---
+++ js/youtube-player/setting.js
@@ -0,0 +1,7 @@
+(function($) {
+	'use strict';
+
+	$('.youTubeEmbed').youTubeEmbed("https://www.youtube.com/watch?v=zzgh62cPiXg");
+$("#playerid").attr("src","");
+$("#playerid").attr("src",video);
+})(jQuery);
