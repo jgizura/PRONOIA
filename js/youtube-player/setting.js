42dbbc77a09f0d7679a68a5d97824015cd8c9617
Matias Izura <lic.matiasizura@gmail.com>
2018-07-17 22:18:30 -0300

    Barrida

--- js/youtube-player/setting.js
+++
@@ -1,7 +0,0 @@
-(function($) {
-	'use strict';
-
-	$('.youTubeEmbed').youTubeEmbed("https://www.youtube.com/watch?v=zzgh62cPiXg");
-$("#playerid").attr("src","");
-$("#playerid").attr("src",video);
-})(jQuery);
